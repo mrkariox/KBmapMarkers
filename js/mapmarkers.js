b955be56e2c429949f7cd8992faad8d44cafbb66
Triv <karol.bartkowski9@gmail.com>
2018-03-27 18:41:22 +0200

    test

--- js/mapmarkers.js
+++ js/mapmarkers.js
@@ -4,6 +4,7 @@ var maxZindex = 2;
 
 function MapMarker(name, icon, container, location_name, jsonfile){
 
+	// test dzialania;
 	this.name = name;
 	this.icon = icon;
 	this.location = location_name;
